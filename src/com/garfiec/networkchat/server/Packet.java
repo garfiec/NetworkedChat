13845a73b3a5b0e1b0e0af0952940f3191187f17
almond <ammarsubei@gmail.com>
2017-12-07 23:12:34 -0600

    Updates Packet

--- src/com/garfiec/networkchat/server/Packet.java
+++ src/com/garfiec/networkchat/server/Packet.java
@@ -13,18 +13,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.math.BigInteger;
 import java.io.Serializable;
+import com.garfiec.networkchat.common.Crypt_RSA.Keys;
 
-public class Packet<T extends Serializable> implements Serializable
+public class Packet implements Serializable
 {
-  private ArrayList< Tuple<String, T> > data;
   private int packetType;
+  private ArrayList< Tuple<String, Keys> > keyData;
+  private ArrayList< Tuple<String, ArrayList<BigInteger>> > messageData;
 
   /**
    *  @brief Default constructor
    */
   public Packet(int type)
   {
-    data = new ArrayList< Tuple<String, T> >();
+    keyData = new ArrayList< Tuple<String, Keys> >();
+    messageData = new ArrayList< Tuple<String, ArrayList<BigInteger>> >();
     packetType = type;
   }
 
@@ -44,17 +47,31 @@ public class Packet<T extends Serializable> implements Serializable
    */
   public String getName(int index)
   {
-    return data.get(index).getFirst();
+    if ( !keyData.isEmpty() ) {
+      return keyData.get(index).getFirst();
+    } else {
+      return messageData.get(index).getFirst();
+    }
+  }
+
+  /**
+   *  @brief Gets key at given index
+   *  @param int index
+   *  @return Keys key
+   */
+  public Keys getKey(int index)
+  {
+    return keyData.get(index).getSecond();
   }
 
   /**
    *  @brief Gets message at given index
    *  @param int index
-   *  @return T message
+   *  @return ArrayList<BigInteger> message
    */
-  public T getMessage(int index)
+  public ArrayList<BigInteger> getMessage(int index)
   {
-    return data.get(index).getSecond();
+    return messageData.get(index).getSecond();
   }
 
   /**
@@ -63,34 +80,39 @@ public class Packet<T extends Serializable> implements Serializable
    */
   public int getSize()
   {
-    return data.size();
-  }
-
-  /**
-   *  @brief Checks if packet is empty
-   *  @return boolean is empty
-   */
-  public boolean isEmpty()
-  {
-    return data.isEmpty();
+    if ( !keyData.isEmpty() ) {
+      return keyData.size();
+    } else {
+      return messageData.size();
+    }
   }
 
   /**
    *  @brief Adds a new entry to the packet
    *  @param String name
-   *  @param T message
+   *  @param Keys key
    */
-  public void add(String name, T newData)
+  public void add(String name, Keys neyKey)
   {
-    data.add( new Tuple<String, T>(name, newData) );
+    keyData.add( new Tuple<String, Keys>(name, neyKey) );
+  }
+
+  /**
+   *  @brief Adds a new entry to the packet
+   *  @param String name
+   *  @param ArrayList<BigInteger> message
+   */
+  public void add(String name, ArrayList<BigInteger> message)
+  {
+    messageData.add( new Tuple<String, ArrayList<BigInteger>>(name, message) );
   }
 
   /**
    *  @brief Appends data to existing packet
    *  @param List<Tuple<String,T>> data to add
    */
-  public void add(List<Tuple<String,T>> newData)
-  {
-    data.addAll(newData);
-  }
+  //public void add(List<Tuple<String,T>> newData)
+  //{
+    //data.addAll(newData);
+  //}
 }
